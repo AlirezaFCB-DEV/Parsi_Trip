8745fe5e560c9674f30ab005c05da6b650fa6e23
sobhan <sobhanjafarii87@gmail.com>
2026-01-02 20:46:20 +0330

    re-install-frontend-project

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 export default function Home() {
+
   return (
-    <>
+    <QueryClientProvider client={new QueryClient()}>
       <h1>hello world</h1>
-    </>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
