b1062a4ce283b6dc690f770a9a3e5201c809d099
sobhan <sobhanjafarii87@gmail.com>
2026-01-06 17:51:32 +0330

    added-next-slide-animation

---
+++ frontend/src/services/Slider/Desktop-slider/hook.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { IdesktopSlide } from "./types";
+
+export default function useDesktopSlider() {
+  return useQuery<IdesktopSlide[]>({
+    queryKey: ["desktop-slider"],
+    queryFn: async () => {
+      const { data } = await axios("http://localhost:8000/desktopSlider");
+
+      return data as IdesktopSlide[];
+    },
+  });
+}
