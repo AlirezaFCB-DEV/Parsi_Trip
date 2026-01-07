0911c234d0da2de44afcecd63e88218208fae1a0
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-01-07 14:33:59 +0330

    feat : add has user in DB feature

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("otp-sender/" , view=views.otp_sender , name="otp_sender")
+    path("otp-sender/" , view=views.otp_sender , name="otp_sender"),
+    path("has-user/" , views.has_user , name="has_user"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,10 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .services import is_email, is_phone , otp_generator
-from .models import OTP
+from django.db.models import Q
 
+from .services import is_email, is_phone, otp_generator
+from .models import OTP, User
 # Create your views here.
 
 
@@ -11,8 +12,7 @@ from .models import OTP
 def otp_sender(req):
     identifier = req.GET.get("identifier")
 
-
-    otp = OTP.objects.create(identifier = identifier, code = otp_generator())
+    otp = OTP.objects.create(identifier=identifier, code=otp_generator())
 
     if is_email(identifier):
         return Response({"data": {"sender_email": identifier, "code": otp.code}})
@@ -23,3 +23,16 @@ def otp_sender(req):
     else:
         return Response({"err": "Please a correct value"},
                         status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def has_user(req):
+    identifier = req.GET.get("identifier")
+
+    if not identifier:
+        return Response({"error": "you must send a query param : identifier=(email | phone)"}, status=status.HTTP_400_BAD_REQUEST)
+
+    exists = User.objects.filter(Q(email=identifier) |
+                               Q(phone_number=identifier)).exists()
+
+    return Response({"exists": True}, status=status.HTTP_200_OK) if exists else Response({"exists": False}, status=status.HTTP_404_NOT_FOUND)
