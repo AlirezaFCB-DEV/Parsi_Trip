c98109e7fb8db05a03f09271c15fc63bf4687011
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-01-12 16:06:49 +0330

    feat : signup system feature

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("otp-sender/" , view=views.otp_sender , name="otp_sender"),
     path("has-user/" , views.has_user , name="has_user"),
-    path("login/" , views.login_view , name="login_view")
+    path("login/" , views.login_view , name="login_view"),
+        path("signup/" , views.signup_view , name="signup_view")
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate , login
+from django.contrib.auth import authenticate, login
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -13,9 +13,9 @@ from .serializers import UserSerializer
 @api_view(["GET"])
 def otp_sender(req):
     identifier = req.GET.get("identifier")
-    
-    if not identifier :
-        return Response({"error" : "you must send a query param : identifier:(email or phone)"} , status=status.HTTP_400_BAD_REQUEST)
+
+    if not identifier:
+        return Response({"error": "you must send a query param : identifier:(email or phone)"}, status=status.HTTP_400_BAD_REQUEST)
 
     otp = OTP.objects.create(identifier=identifier, code=otp_generator())
 
@@ -37,40 +37,74 @@ def has_user(req):
     if not identifier:
         return Response({"error": "you must send a query param : identifier=(email | phone)"}, status=status.HTTP_400_BAD_REQUEST)
 
-    if is_email(identifier) or is_phone(identifier) :
+    if is_email(identifier) or is_phone(identifier):
         exists = User.objects.filter(Q(email=identifier) |
-                               Q(phone_number=identifier)).exists()
-    else :
-        return Response({"error" : "identifier is incorrect!!"} , status=status.HTTP_400_BAD_REQUEST)
+                                     Q(phone_number=identifier)).exists()
+    else:
+        return Response({"error": "identifier is incorrect!!"}, status=status.HTTP_400_BAD_REQUEST)
 
     return Response({"exists": True}, status=status.HTTP_200_OK) if exists else Response({"exists": False}, status=status.HTTP_404_NOT_FOUND)
 
+
 @api_view(["POST"])
-def login_view(req) :
+def login_view(req):
     identifier = req.data.get("identifier")
     otp = req.data.get("otp")
-    password= req.data.get("password")
-    
-    if not identifier :
-        return Response({"error" : "Identifier is required!"} , status=status.HTTP_400_BAD_REQUEST)
-    
-    if not otp and not password :
-        return Response({"error" : "Either OTP or password is required"} , status=status.HTTP_400_BAD_REQUEST)
-    
-    if not (is_email(identifier) or  is_phone(identifier)) :
-        return Response({"error" : "Identifier must be a valid email or phone."} , status=status.HTTP_400_BAD_REQUEST)
-    
+    password = req.data.get("password")
+
+    if not identifier:
+        return Response({"error": "Identifier is required!"}, status=status.HTTP_400_BAD_REQUEST)
+
+    if not otp and not password:
+        return Response({"error": "Either OTP or password is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+    if not (is_email(identifier) or is_phone(identifier)):
+        return Response({"error": "Identifier must be a valid email or phone."}, status=status.HTTP_400_BAD_REQUEST)
+
     if otp:
-        auth_result = authenticate(req , identifier=identifier , otp=otp )
+        auth_result = authenticate(req, identifier=identifier, otp=otp)
         error_msg = "Invalid OTP"
-    else :
-        auth_result = authenticate(req , identifier=identifier , password=password )
+    else:
+        auth_result = authenticate(
+            req, identifier=identifier, password=password)
         error_msg = "Invalid Password"
-    
-    
-    if auth_result :
-        login(req , auth_result)
+
+    if auth_result:
+        login(req, auth_result)
         serializer = UserSerializer(auth_result)
-        return Response({"msg" : "user logged successfully" , "user" : serializer.data} , status=status.HTTP_200_OK)
+        return Response({"msg": "user logged successfully", "user": serializer.data}, status=status.HTTP_200_OK)
+
+    return Response({"error": error_msg}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def signup_view(req):
+    email = req.data.get("email")
+    phone_number = req.data.get("phone_number")
+    user_fullname = req.data.get("user_fullname")
+    password = req.data.get("password")
+
+    if not email:
+        return Response({"error": "you must send email"}, status=status.HTTP_400_BAD_REQUEST)
+    elif not phone_number:
+        return Response({"error": "you must send phone number"}, status=status.HTTP_400_BAD_REQUEST)
+    elif not user_fullname:
+        return Response({"error": "you must send user fullname"}, status=status.HTTP_400_BAD_REQUEST)
+    elif not password:
+        return Response({"error": "you must send password"}, status=status.HTTP_400_BAD_REQUEST)
+
+    if not is_email(email):
+        return Response({"error": "you must send a valid email"}, status=status.HTTP_400_BAD_REQUEST)
+    elif not is_phone(phone_number):
+        return Response({"error": "you must send a valid phone"}, status=status.HTTP_400_BAD_REQUEST)
+    elif len(user_fullname) > 150:
+        return Response({"error": "user fullname must be lower than 150 characters"}, status=status.HTTP_400_BAD_REQUEST)
+    elif len(password) <  8:
+        return Response({"error": "password must be longer than 8 characters"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(
+        email=email, phone_number=phone_number, user_fullname=user_fullname, password=password)
     
-    return Response({"error" : error_msg} , status=status.HTTP_400_BAD_REQUEST)
+    serializer = UserSerializer(user)
+    
+    return Response({"msg" : "user created successfully" , "user" : serializer.data} , status=status.HTTP_200_OK)
