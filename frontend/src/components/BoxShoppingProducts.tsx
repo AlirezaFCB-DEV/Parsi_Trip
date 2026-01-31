203056f7131bb8baa05b914955748d59e139a081
sobhan <sobhanjafarii87@gmail.com>
2026-01-31 13:48:06 +0330

    finished-all-shopping-box-products

--- frontend/src/components/BoxShoppingProducts.tsx
+++ frontend/src/components/BoxShoppingProducts.tsx
@@ -6,14 +6,12 @@ import IboxShoppingProduct from "@/services/Box-shopping-products/types";
 import useBoxShoppingProductTwo from "@/services/Box-shopping-products/Box-shopping-product-two/hook";
 import useBoxShoppingProductThree from "@/services/Box-shopping-products/Box-shopping-product-three/hook";
 import useBoxShoppingProductFour from "@/services/Box-shopping-products/Box-shopping-product-four/hook";
-import useBoxShoppingProductFive from "@/services/Box-shopping-products/Box-shopping-product-five/hook";
 
 function BoxShoppingProducts() {
   const { data: dataOne = [] } = useBoxShoppingProductOne();
   const { data: dataTwo = [] } = useBoxShoppingProductTwo();
   const { data: dataThree = [] } = useBoxShoppingProductThree();
   const { data: dataFour = [] } = useBoxShoppingProductFour();
-  const { data: dataFive = [] } = useBoxShoppingProductFive();
 
   return (
     <div className="flex flex-col justify-between items-center">
@@ -37,12 +35,6 @@ function BoxShoppingProducts() {
 
       <div className="w-9/11 h-100 hidden xl:flex justify-between items-center gap-5 container p-5  mx-auto -translate-y-50">
         {dataFour.map((item: IboxShoppingProduct) => (
-          <BoxShoppingProduct key={item.id} {...item} />
-        ))}
-      </div>
-
-      <div className="w-9/11 h-100 hidden xl:flex justify-between items-center gap-5 container p-5  mx-auto -translate-y-50">
-        {dataFive.map((item: IboxShoppingProduct) => (
           <BoxShoppingProduct key={item.id} {...item} />
         ))}
       </div>
