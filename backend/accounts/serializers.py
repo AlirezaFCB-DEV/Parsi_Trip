e4fb10efb33aa50653a24f609667579ae21dea87
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-02-06 12:33:20 +0330

    feat(config) : config settings and handling short bugs

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -2,8 +2,6 @@ import phonenumbers
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-from .models import Address
-
 class UserSerializer(serializers.ModelSerializer) :
     class Meta:
         model = get_user_model()
