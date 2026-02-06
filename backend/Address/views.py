7a12e3857979a24d442a481bdf4d9f8b6df08904
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-02-06 12:26:59 +0330

    feat(Address.views) : move AddressViewSet from accounts.views to Address.views

--- backend/Address/views.py
+++ backend/Address/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Address
+from .serializers import AddressSerializer
 
 # Create your views here.
+class AddressViewSet(viewsets.ModelViewSet):        
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Address.objects.filter(owner=self.request.user)
+    
