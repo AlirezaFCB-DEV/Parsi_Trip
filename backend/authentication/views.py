2a88d7e188fb8dd7d4cd8f83c1ab4d2cc2bb33d0
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-02-12 22:44:14 +0330

    feat(accounts.views) : create exsits action in UserViewSet

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -28,44 +28,6 @@ class OTPSender(APIView) :
             "sender_identifier" :identifier , "code" : otp.code
         }} , status=201)
         
-        
-
-# @api_view(["GET"])
-# def otp_sender(req):
-#     identifier = req.GET.get("identifier")
-
-#     if not identifier:
-#         return Response({"error": "you must send a query param : identifier:(email or phone)"}, status=status.HTTP_400_BAD_REQUEST)
-
-#     otp = OTP.objects.create(identifier=identifier, code=otp_generator())
-
-#     if is_email(identifier):
-#         return Response({"data": {"sender_email": identifier, "code": otp.code}})
-
-#     elif is_phone(identifier):
-#         return Response({"data": {"sender_phone": identifier, "code": otp.code}})
-
-#     else:
-#         return Response({"err": "identifier is incorrect!!"},
-#                         status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(["GET"])
-# def has_user(req):
-#     identifier = req.GET.get("identifier")
-
-#     if not identifier:
-#         return Response({"error": "you must send a query param : identifier=(email | phone)"}, status=status.HTTP_400_BAD_REQUEST)
-
-#     if is_email(identifier) or is_phone(identifier):
-#         exists = User.objects.filter(Q(email=identifier) |
-#                                      Q(phone_number=identifier)).exists()
-#     else:
-#         return Response({"error": "identifier is incorrect!!"}, status=status.HTTP_400_BAD_REQUEST)
-
-#     return Response({"exists": True}, status=status.HTTP_200_OK) if exists else Response({"exists": False}, status=status.HTTP_404_NOT_FOUND)
-
-
 # @api_view(["POST"])
 # def login_view(req):
 #     identifier = req.data.get("identifier")
