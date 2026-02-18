0bacbfb26ef3367f31e2fdc781ad13dade0700c9
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-02-18 16:14:59 +0330

    update(config.services) : update verify_otp service

--- backend/config/services/verify_otp.py
+++ backend/config/services/verify_otp.py
@@ -3,27 +3,28 @@ from django.utils import timezone
 
 from authentication.models import OTP
 
-def verify_otp(identifier , otp_code) :
-        try :
-            otp_record = OTP.objects.get(identifier=identifier)
-            
-            if otp_record.code == otp_code :
-                
-                if otp_record.created_at + timedelta(minutes=2) < timezone.now():
-                    otp_record.delete()
-                    return False
-                
-                otp_record.delete()
-                return True     
-            
-            else :
-                otp_record.attempts += 1
-                otp_record.save()
-                
-                if otp_record.attempts >= 3:
-                    otp_record.delete()
-                
-                return False
-            
-        except OTP.DoesNotExist :
-                return False
+
+def verify_otp(identifier, otp_code):
+    try:
+        otp_record = OTP.objects.filter(
+            identifier=identifier).latest("created_at")
+
+    except OTP.DoesNotExist:
+        return False
+
+    if otp_record.created_at + timedelta(minutes=2) < timezone.now():
+        otp_record.delete()
+        return False
+
+    if otp_record.code == otp_code:
+
+        otp_record.delete()
+        return True
+
+    otp_record.attempts += 1
+    otp_record.save()
+
+    if otp_record.attempts >= 3:
+        otp_record.delete()
+
+    return False
