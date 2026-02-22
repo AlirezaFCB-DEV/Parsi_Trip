873e1ee705e0801e98a5e69121f36b6e096f10a0
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-02-22 11:04:48 +0330

    feat(catalog.models) : create Product Model

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -2,3 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+class Product(models.Model) :
+    category = models.ForeignKey(model="Category" , on_delete=models.PROTECT , related_name="products")
+    brand = models.ForeignKey(model = "Brand" , on_delete=models.SET_NULL , null=True , blank=True) 
+    
+    weight = models.DecimalField(max_digits=8 , decimal_places=2 , null=True , blank=True)
+    sku = models.CharField(max_length=50 , unique=True)
+    discount_percent = models.PositiveIntegerField(null=True , blank=True)
+    
+    title = models.CharField(max_length=255) #! Field
+    slug = models.SlugField(unique=True) #! url field
+    
+    description = models.TextField() #! Desc
+    category = models.ForeignKey() #! category
+    
+    price = models.DecimalField(max_digits=12, decimal_places=2) #! price
+    stock = models.PositiveIntegerField(default=0) #! in stock
+    
+    is_active = models.BooleanField(default=True) #! is active
+    
+    created_at = models.DateTimeField(auto_now_add=True) #! date created
+    updated_at = models.DateTimeField(auto_now=True) #! updated at
