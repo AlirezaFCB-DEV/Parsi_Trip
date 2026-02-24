8601f815009c5fb8c2ce88167f419c3a836c60bf
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2026-02-24 19:23:40 +0330

    feat(catalog.models) : create CategoryAttribute model

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -14,7 +14,6 @@ class Product(models.Model) :
     slug = models.SlugField(unique=True) #! url field
     
     description = models.TextField() #! Desc
-    category = models.ForeignKey() #! category
     
     price = models.DecimalField(max_digits=12, decimal_places=2) #! price
     stock = models.PositiveIntegerField(default=0) #! in stock
@@ -35,7 +34,7 @@ class ProductAttribute(models.Model) :
         ("integer" , "Integer")
     ]
     
-    name = models.CharField(max_length=255 , unique=True)
+    title = models.CharField(max_length=255 , unique=True)
     label = models.CharField(max_length=255)
     data_type = models.CharField(choices=data_type_choices , max_length=30)
     is_filterable = models.BooleanField(default=False)
@@ -47,16 +46,10 @@ class ProductAttribute(models.Model) :
 class Category(models.Model) :
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255,unique=True)
-    
     parent = models.ForeignKey("self" , null=True , blank=True, related_name="children" , on_delete=models.CASCADE)
-    
-    
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to="categories/" , null=True ,blank=True)
-    
     is_active = models.BooleanField(default=True)
-    display_order = models.IntegerField(default=0)
-    
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     
@@ -65,3 +58,16 @@ class Category(models.Model) :
     
     def __str__(self):
         return self.title
+    
+class CategoryAttribute(models.Model) :
+    category = models.ForeignKey(Category , on_delete=models.PROTECT , db_index=True)
+    attribute = models.ForeignKey(ProductAttribute , on_delete=models.PROTECT , db_index=True)
+    is_required = models.BooleanField(default=False)
+    display_order = models.IntegerField(default=1)
+    
+    class Meta : 
+        unique_together = ("category" , "attribute") #* unique Fields
+        ordering = ["display_order"]
+        
+    def __str__(self):
+        return f"{self.category.title} - {self.attribute.title}"
